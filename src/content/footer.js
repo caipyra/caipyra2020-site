f6c450669ae7f60c78abf86a3b03d42ef96c01c9
Arthur Fortes <fortes.arthur@gmail.com>
2020-01-21 21:11:02 -0300

    add info

--- src/content/footer.js
+++ src/content/footer.js
@@ -2,15 +2,19 @@ module.exports = {
     nav: [{
             title: 'Sobre',
             items: [
-                { text: 'Local', link: 'https://goo.gl/maps/m7ttyu5CCnBjobmD7' },
-                { text: 'Keynotes', link: '#section-keynote' },
+                { text: 'Local', link: '#section-locations'},
                 { text: 'Evento', link: '#section-about' },
-                { text: 'Guia da Cidade', link: '#section-cityguide' },
-                // { text: 'Programação', link: '#' },
+                { text: 'Programação', link: '#section-lineup' },
                 { text: 'Código de Conduta', link: 'https://python.org.br/cdc/' },
             ]
         },
-
+        {
+            title: 'Patrocínio',
+            items: [
+                { text: 'Quero Patrocinar', link: 'mailto:contato.caipyra@gmail.com', type: '_blank'},
+                { text: 'Patrocinadores', link: '#section-sponsors' },
+            ]
+        },
     ],
     social: {
         facebook: { link: 'https://www.facebook.com/caipyra/', logo: '/images/facebook.svg' },
